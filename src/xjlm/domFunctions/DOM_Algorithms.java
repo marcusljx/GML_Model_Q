bb3aff264da6c4d31e3910b936194f58f1ec3f4f
marcusljx <marcusljx@gmail.com>
2016-02-20 12:19:11 +0800

    Moved tree-structure functions from gml_Parser to DOM_Algorithms

--- src/xjlm/domFunctions/DOM_Algorithms.java
+++ src/xjlm/domFunctions/DOM_Algorithms.java
@@ -3,6 +3,7 @@ package xjlm.domFunctions;
 import org.dom4j.Element;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by marcusljx on 19/02/16.
@@ -10,6 +11,50 @@ import java.util.ArrayList;
  */
 
 public class DOM_Algorithms {
+
+    //===============================================
+    //=================== STRING ====================
+    //===============================================
+    public static String getFullNameOfNode(Element node) {
+        return (node.getNamespacePrefix().equals("")) ? node.getName() : node.getNamespacePrefix() + ":" + node.getName();
+    }
+
+    public String trimNamespaces(String S) {
+        return S.replaceAll("([A-Za-z0-9]*:)", "");
+    }
+
+    public static ArrayList<String> getListOfPathsEndingWith(Element node, String name) {   // local recursion stops at first found
+        ArrayList<String> result = new ArrayList<>();
+
+        String prefix = node.getNamespacePrefix();
+        String nName;
+        if(!prefix.equals("")) {
+            nName = prefix + ":" + node.getName();
+        } else {
+            nName = node.getName();
+        }
+
+        if(nName.equals(name)) {
+            result.add("/"+ nName);
+        } else {
+            // Traverse Children recursively. Append their results together;
+            List<Element> children = node.elements();
+            for(Element c : children) {
+                result.addAll(getListOfPathsEndingWith(c, name));
+            }
+
+            // Add own path in front of all results
+            for(int i=0; i<result.size(); i++) {
+                result.set(i, "/" + nName + result.get(i));
+            }
+        }
+
+        return result;
+    }
+
+    //===============================================
+    //================= TREE SEARCH =================
+    //===============================================
     public static Element LowestCommonParent(ArrayList<Element> children) {
         ArrayList<Element> result = new ArrayList<>();
         result.addAll(children);
@@ -34,5 +79,39 @@ public class DOM_Algorithms {
         }
 
         return result.get(0);
+    }
+
+    public static ArrayList<Element> getNodesWithName(Element node, String name) {
+        ArrayList<Element> result = new ArrayList<>();
+        if(getFullNameOfNode(node).equals(name)) {
+            result.add(node);
+        } else {
+            List<Element> children = node.elements();
+            for(Element e : children) {
+                result.addAll(getNodesWithName(e, name));
+            }
+        }
+
+        return result;
+    }
+
+    // returns list of descendants matching a descendantPath from the A-th sibling named branchPath
+    public ArrayList<Element> get_subnodes_from_branchA(Element node, String branchPath, int A, String descendantPath) {
+        List<Element> parentCandidates = node.selectNodes(trimNamespaces(branchPath));
+        if(parentCandidates.size() == 1) {
+            A = 1;
+        } else if(parentCandidates.size() < 1) {
+            return null;
+        }
+
+        // Number of candidates > 1
+        Element MajorParent = parentCandidates.get(A);
+
+        // Find
+        System.out.println(getFullNameOfNode(MajorParent));
+//        MajorParent.selectNodes()
+
+        ArrayList<Element> result = new ArrayList<>();
+        return result;
     }
 }
--- src/xjlm/gml_model_Q/gml_Parser.java
+++ src/xjlm/gml_model_Q/gml_Parser.java
@@ -2,6 +2,7 @@ package xjlm.gml_model_Q;
 
 import org.dom4j.*;
 import org.dom4j.io.SAXReader;
+import xjlm.domFunctions.DOM_Algorithms;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -71,84 +72,13 @@ public class gml_Parser {
         }
     }
 
-    public String trimNamespaces(String S) {
-        return S.replaceAll("([A-Za-z0-9]*:)", "");
-    }
-
     //=================== GETTERS
     public Element getRoot() {
         return root;
     }
 
-    public String getFullNameOfNode(Element node) {
-        return (node.getNamespacePrefix().equals("")) ? node.getName() : node.getNamespacePrefix() + ":" + node.getName();
-    }
-
     public Document getDocument() {
         return document;
-    }
-
-    public ArrayList<String> getListOfPathsEndingWith(Element node, String name) {   // local recursion stops at first found
-        ArrayList<String> result = new ArrayList<>();
-
-        String prefix = node.getNamespacePrefix();
-        String nName;
-        if(!prefix.equals("")) {
-            nName = prefix + ":" + node.getName();
-        } else {
-            nName = node.getName();
-        }
-
-        if(nName.equals(name)) {
-            result.add("/"+ nName);
-        } else {
-            // Traverse Children recursively. Append their results together;
-            List<Element> children = node.elements();
-            for(Element c : children) {
-                result.addAll(getListOfPathsEndingWith(c, name));
-            }
-
-            // Add own path in front of all results
-            for(int i=0; i<result.size(); i++) {
-                result.set(i, "/" + nName + result.get(i));
-            }
-        }
-
-        return result;
-    }
-
-    public ArrayList<Element> getNodesWithName(Element node, String name) {
-        ArrayList<Element> result = new ArrayList<>();
-        if(getFullNameOfNode(node).equals(name)) {
-            result.add(node);
-        } else {
-            List<Element> children = node.elements();
-            for(Element e : children) {
-                result.addAll(getNodesWithName(e, name));
-            }
-        }
-
-        return result;
-    }
-
-    // returns list of descendants matching a descendantPath from the A-th sibling named branchPath
-    public ArrayList<Element> get_subnodes_from_branchA(String branchPath, int A, String descendantPath) {
-        List<Element> parentCandidates = root.selectNodes(trimNamespaces(branchPath));
-        if(parentCandidates.size() == 1) {
-            A = 1;
-        } else if(parentCandidates.size() < 1) {
-            return null;
-        }
-
-        // Number of candidates > 1
-        Element MajorParent = parentCandidates.get(A);
-
-        // Find
-        System.out.println(getFullNameOfNode(MajorParent));
-//        MajorParent.selectNodes()
-
-        ArrayList<Element> result = new ArrayList<>();
-        return result;
     }
 
     //==================== OVERRIDES
@@ -157,10 +87,10 @@ public class gml_Parser {
     }
 
     public ArrayList<Element> getNodesWithName(String name) {
-        return getNodesWithName(root, name);
+        return DOM_Algorithms.getNodesWithName(root, name);
     }
 
     public ArrayList<String> getListOfPathsEndingWith(String name) {
-        return getListOfPathsEndingWith(root, name);
+        return DOM_Algorithms.getListOfPathsEndingWith(root, name);
     }
 }
