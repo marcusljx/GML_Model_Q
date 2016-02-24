300d230a79e7af86db46f96483f1382c1be3995e
marcusljx <marcusljx@gmail.com>
2016-02-24 22:19:24 +0800

    Added functionality to find surface normal, and also to determine which of the 3 surface categories it falls under

--- src/gml_model_repair/gml_PolygonSurface.java
+++ src/gml_model_repair/gml_PolygonSurface.java
@@ -1,6 +1,6 @@
 package gml_model_repair;
 
-import javafx.geometry.Point3D;
+import com.sun.javafx.geom.Vec3d;
 
 import java.util.ArrayList;
 
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * Created by marcusljx on 24/02/16.
  */
 public class gml_PolygonSurface {
-    private ArrayList<Point3D> posList = new ArrayList<>();
+    private ArrayList<Vec3d> posList = new ArrayList<>();
 
     public gml_PolygonSurface() {}
     public gml_PolygonSurface(String str_posList) {
@@ -23,15 +23,44 @@ public class gml_PolygonSurface {
             double y = Double.parseDouble(values[i+1]);
             double z = Double.parseDouble(values[i+2]);
 
-            posList.add(new Point3D(x,y,z));
+            posList.add(new Vec3d(x,y,z));
         }
         return this;
     }
 
+    public Vec3d get_surfaceNormal() {
+        // Use 1st 3 coordinates
+        Vec3d U = new Vec3d(posList.get(1));
+        U.sub(posList.get(0));
+
+        Vec3d V = new Vec3d(posList.get(2));
+        V.sub(posList.get(0));
+
+        Vec3d result = new Vec3d();
+        result.cross(U, V);
+        result.normalize();
+
+        return result;
+    }
+
+    public String get_surfaceType() {
+        Vec3d normal = get_surfaceNormal();
+        long rounded_z = Math.round(normal.z);
+
+        if (rounded_z == 1) {
+            return "RoofSurface";
+        } else if (rounded_z == -1) {
+            return "GroundSurface";
+        } else {
+            return "WallSurface";
+        }
+    }
+
+
     @Override
     public String toString() {
         String result = "[" + posList.size() + " points]\n";
-        for(Point3D p : posList) {
+        for(Vec3d p : posList) {
             result += p.toString() + "\n";
         }
         return result;
